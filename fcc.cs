4c00046e208d79b0f0a0002d2b9b58b4b4dfd0ab
Kara Bayse <karabayse@gmail.com>
2019-11-17 16:01:23 -0600

    variables

--- fcc.cs
+++ fcc.cs
@@ -21,6 +21,26 @@ namespace Giraffe
     This code gets executed when we run the Program
     This code runs automatically
     */
-    
+    static void Main(string[] args)
+    {
+      // This line writes to the console
+      Console.WriteLine("Hello, World!");
+      // This line keeps the console window up
+      Console.ReadLine();
+
+      // Variable
+      // A variable is a data store
+      string characterName = "John";
+      // Declares the variable without giving it a value
+      int characterAge;
+      // Assigns a value to the variable
+      characterAge = 35;
+
+      Console.WriteLine("There once was a man name " + characterName);
+      Console.WriteLine("He was " + characterAge + " years old");
+      characterName = "Tom";
+      Console.WriteLine("He really like the name " + characterName);
+      Console.WriteLine("But he didn't like being " + characterAge);
+    }
   }
 }
