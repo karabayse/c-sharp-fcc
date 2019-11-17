9312b8e3636a48a117e08f8f138450120f12cb42
Kara Bayse <karabayse@gmail.com>
2019-11-17 15:56:02 -0600

    initial commit

---
+++ fcc.cs
@@ -0,0 +1,26 @@
+// Commands that configure the file
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+States that we are in the Giraffe project
+Within the Visual Studio IDE, this would match the project name
+*/
+namespace Giraffe
+{
+  // The class is a container where we can store a bunch of code
+  class Program
+  {
+    /*
+    Main is a method or function
+    This is another container where we can put code
+    This is where we put the code that we want to execute
+    This code gets executed when we run the Program
+    This code runs automatically
+    */
+    
+  }
+}
