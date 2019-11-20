82324435d8fdc4111bfbadda29c9d900bf54263b
Kara Bayse <karabayse@gmail.com>
2019-11-19 18:15:01 -0600

    added guessing game file

---
+++ guessing-game.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+      static void Main(string[] args)
+      {
+        int guesses = 0;
+
+
+        Console.ReadLine();
+      }
+    }
+}
